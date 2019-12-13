ef882a7aae3307ca552dd03e9eae7bacd32e22d3
Kanersps <kanepetra@gmail.com>
2019-12-13 08:26:47 +0100

    Add convars

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config	= {}
-Config.ShowID = true -- true/false to set the player source ID showing on HUD
-Config.AdminCommands = true -- Enable/Disable Admin Commands
-Config.RevealMap = true -- REVEAL ALL OF THE MAP
-Config.Coords = true -- SHOW COORDS
-Config.ShowXPandLevel = true -- true/false to set the player XP AND LEVEL on HUD
-Config.pvp = true -- true/false to enable or disable PVP
+Config.ShowID = GetConvarInt("redemrp_showid", 1) -- true/false to set the player source ID showing on HUD
+Config.AdminCommands = GetConvarInt("redemrp_admincommands", 1) -- Enable/Disable Admin Commands
+Config.RevealMap = GetConvarInt("redemrp_revealmap", 1) -- REVEAL ALL OF THE MAP
+Config.Coords = GetConvarInt("redemrp_showcoords", 1) -- SHOW COORDS
+Config.ShowXPandLevel = GetConvarInt("redemrp_showlevel", 1) -- true/false to set the player XP AND LEVEL on HUD
+Config.pvp = GetConvarInt("redemrp_enablepvp", 1) -- true/false to enable or disable PVP
