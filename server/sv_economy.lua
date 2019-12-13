c5b72b0ec5f5f035aad1feb3b25bbe89e8344521
Kanersps <kanepetra@gmail.com>
2019-12-13 19:14:19 +0100

    Fix some weird indentation

--- server/sv_economy.lua
+++ server/sv_economy.lua
@@ -1,148 +1,92 @@
 if Config.AdminCommands == 1 then
-RegisterCommand("setgold", function(source, args, rawCommand)
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
- 
- TriggerEvent("redemrp:SetSomeGold", source, args[1], args[2], function(cb)
- print("Gold Setted")
- end)
- 
+	RegisterCommand("setgold", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:SetSomeGold", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
- end
-end, false)
+	RegisterCommand("setmoney", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:SetSomeMoney", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
-RegisterCommand("setmoney", function(source, args, rawCommand)
+	RegisterCommand("setlevel", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:SetSomeLevel", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:SetSomeMoney", source, args[1], args[2], function(cb)
- print("Money Setted")
- end)
- 
+	RegisterCommand("setxp", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:SetSomeXP", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
- end
-end, false)
+	RegisterCommand("setjob", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil or args[3] == nil then
+			print("argument #1, #2 or #3 is bad")
+		else
+			TriggerEvent("redemrp:SetSomeJob", source, args[1], args[2], args[3], function(cb)end)
+		end
+	end, false)
 
-RegisterCommand("setlevel", function(source, args, rawCommand)
+	RegisterCommand("addmoney", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:AddSomeMoney", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:SetSomeLevel", source, args[1], args[2], function(cb)
- print("Level Setted")
- end)
- 
+	RegisterCommand("addgold", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:AddSomeGold", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
- end
-end, false)
+	RegisterCommand("addxp", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:AddSomeXP", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
-RegisterCommand("setxp", function(source, args, rawCommand)
+	RegisterCommand("removemoney", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:RemoveSomeMoney", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:SetSomeXP", source, args[1], args[2], function(cb)
- print("XP Setted")
- end)
- 
+	RegisterCommand("removegold", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:RemoveSomeGold", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
 
- end
-end, false)
-
-RegisterCommand("setjob", function(source, args, rawCommand)
-    if args[1] == nil or args[2] == nil or args[3] == nil then
-    print("argument #1 or #2 or #3 is bad")
-    else
-    
-        TriggerEvent("redemrp:SetSomeJob", source, args[1], args[2], args[3], function(cb)
-    print("Job Setted")
-    end)
-    
-   
-    end
-   end, false)
-
-RegisterCommand("addmoney", function(source, args, rawCommand)
-
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:AddSomeMoney", source, args[1], args[2], function(cb)
- print("Money Added")
- end)
- 
-
- end
-end, false)
-
-
-RegisterCommand("addgold", function(source, args, rawCommand)
-
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:AddSomeGold", source, args[1], args[2], function(cb)
- print("Gold Added")
- end)
- 
-
- end
-end, false)
-
-RegisterCommand("addxp", function(source, args, rawCommand)
-
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:AddSomeXP", source, args[1], args[2], function(cb)
- print("XP Added")
- end)
- 
-
- end
-end, false)
-
-RegisterCommand("removemoney", function(source, args, rawCommand)
-
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:RemoveSomeMoney", source, args[1], args[2], function(cb)
- print("Money Removed")
- end)
- 
-
- end
-end, false)
-
-RegisterCommand("removegold", function(source, args, rawCommand)
-
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:RemoveSomeGold", source, args[1], args[2], function(cb)
- print("Gold Removed")
- end)
- 
-
- end
-end, false)
-
-RegisterCommand("setgroup", function(source, args, rawCommand)
-
- if args[1] == nil or args[2] == nil then
- print("argument #1 or #2 is bad")
- else
-    TriggerEvent("redemrp:SetSomeGroup", source, args[1], args[2], function(cb)
- print("Group Setted")
- end)
- 
-
- end
-end, false)
-else end
+	RegisterCommand("setgroup", function(source, args, rawCommand)
+		if args[1] == nil or args[2] == nil then
+			print("argument #1 or #2 is bad")
+		else
+			TriggerEvent("redemrp:SetSomeGroup", source, args[1], args[2], function(cb)end)
+		end
+	end, false)
+end
 
 AddEventHandler("redemrp:SetSomeGold", function(source, id, count, cb)
 	local _perm = tonumber(source)
