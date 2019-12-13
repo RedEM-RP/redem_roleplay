0920f44bd67ceb600a06317f9575687d415b14ac
Kanersps <kanepetra@gmail.com>
2019-12-13 19:25:17 +0100

    Remove bad files

--- server/sv_callback.lua
+++
@@ -1,7 +0,0 @@
-RegisterServerEvent('redemrp:callback')
-AddEventHandler('redemrp:callback', function(id, name, data, cb)
-    local _source = source
-    TriggerEvent(name, data, function(cb)
-        TriggerClientEvent('redemrp:callbackReturn', _source, id, cb)
-    end)
-end)
