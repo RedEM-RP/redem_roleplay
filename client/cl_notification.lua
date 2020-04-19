f708e9094ad70644778140ae3538abea289c12d6
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-04-20 01:02:00 +0200

    dict

--- client/cl_notification.lua
+++ client/cl_notification.lua
@@ -6,16 +6,15 @@
 -- exports.redem_roleplay.DisplayTopCenterNotification(0, "your text", tonumber(8000))
 -- exports.redem_roleplay.DisplayLeftNotification(0, "your text", "second text", "generic_textures", "tick", tonumber(8000))
 -- OR JUST
--- TriggerClientEvent('redem_roleplay:NotifyLeft', "first text", "second text", "tick", 8000)
+-- TriggerClientEvent('redem_roleplay:NotifyLeft', "first text", "second text", "generic_textures", "tick", 8000)
 -- TriggerClientEvent('redem_roleplay:Tip', "your text", 8000)
 -- TriggerClientEvent('redem_roleplay:NotifyTop', "your text", 8000)
 
 RegisterNetEvent('redem_roleplay:NotifyLeft')
-AddEventHandler('redem_roleplay:NotifyLeft', function(firsttext, secondtext, icon, duration)
-local dict = "generic_textures"
-    if not HasStreamedTextureDictLoaded("generic_textures") then
-        RequestStreamedTextureDict("generic_textures", true) 
-            while not HasStreamedTextureDictLoaded("generic_textures") do
+AddEventHandler('redem_roleplay:NotifyLeft', function(firsttext, secondtext, dict, icon, duration)
+    if not HasStreamedTextureDictLoaded(dict) then
+        RequestStreamedTextureDict(dict, true) 
+            while not HasStreamedTextureDictLoaded(dict) do
                 Wait(0)
             end
     end
