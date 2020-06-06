83c4ffa2cb49d76b2f2739876dbeff42bae2cf05
Kane <kanersps@pm.me>
2020-06-06 14:00:38 +0200

    tweak: increase version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version "adamant"
 games {"rdr3"}
+version '1.0.0'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
