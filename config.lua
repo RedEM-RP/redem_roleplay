616cefebc755dc1f96fda079cef8a7ca6c08ca7d
ALTITUDE DEV <egamestudio@outlook.com>
2022-03-14 23:39:07 +0100

    Add config debugs and clean file

--- config.lua
+++ config.lua
@@ -1,8 +1,10 @@
-Config	= {}
-Config.ShowID = GetConvarInt("redemrp_showid", 1) -- true/false to set the player source ID showing on HUD
-Config.AdminCommands = GetConvarInt("redemrp_admincommands", 1) -- Enable/Disable Admin Commands
-Config.RevealMap = GetConvarInt("redemrp_revealmap", 1) -- REVEAL ALL OF THE MAP
-Config.Coords = GetConvarInt("redemrp_showcoords", 1) -- SHOW COORDS
-Config.ShowXPandLevel = GetConvarInt("redemrp_showlevel", 1) -- true/false to set the player XP AND LEVEL on HUD
-Config.pvp = GetConvarInt("redemrp_enablepvp", 1) -- true/false to enable or disable PVP
-Config.EntityDistance = GetConvarInt("redemrp_entitydistance", 50.0)
+Config	                  = {}
+
+Config.ShowID             = GetConvarInt("redemrp_showid", 1)             -- true/false to set the player source ID showing on HUD
+Config.AdminCommands      = GetConvarInt("redemrp_admincommands", 1)      -- Enable/Disable Admin Commands
+Config.RevealMap          = GetConvarInt("redemrp_revealmap", 1)          -- Reveal all the map
+Config.Coords             = GetConvarInt("redemrp_showcoords", 1)         -- Show coords in screen
+Config.ShowXPandLevel     = GetConvarInt("redemrp_showlevel", 1)          -- true/false to set the player XP AND LEVEL on HUD
+Config.pvp                = GetConvarInt("redemrp_enablepvp", 1)          -- true/false to enable or disable PVP
+Config.EntityDistance     = GetConvarInt("redemrp_entitydistance", 50.0)  -- 
+Config.Debuglogs          = true                                          -- Enable console logs (For developpers) (in production turn off, for clean console logs)
